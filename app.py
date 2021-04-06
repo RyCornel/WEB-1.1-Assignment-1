d7c77888296aecc48bd64676cbabd3e7b62ce824
Ryan Cornel <ry.cornel@students.makeschool.com>
2021-04-06 11:53:44 -0700

    Commit 3
    Created dessert app.route & favorite_dessert function.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask 
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def homepage():
@@ -11,6 +11,10 @@ def homepage():
 def favorite_animal(users_animal):
     """Display a message to the user that changes based on their favorite animal"""
     return f"Wow, {users_animal} is my favorite animal, too!"
+
+@app.route('/dessert/<users_dessert>')
+def favorite_dessert(users_dessert):
+    return f"How did you know I liked {users_dessert}?"
 
 
 if __name__ == '__main__':
