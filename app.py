3d710ea5044310bd3fc5133f3a8c78850659ea43
Ryan Cornel <ry.cornel@students.makeschool.com>
2021-04-06 11:27:14 -0700

    Commit 1
    Added app.route & homepage function.

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 from flask import Flask 
 
 app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    """Shows a greeting to the user. """
+    return "Are you there, world? It's me, Ducky!"
+
+
+
+    if __name__ == '__main__':
+        app.run(debug=True)
